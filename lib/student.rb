81756c05fd6f7c2cc459cd9115c5876be17e030b
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-23 17:14:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,17 +1,18 @@
 class Student
-  attr_accessor :name ,:grade 
-  attr_reader :id 
+  attr_accessor :name ,:grade
+  attr_reader :id
 
   def initialize ()
 
-  end 
+  end
 
-  def self.create_table 
-    sql = <<-SQL 
+  def self.create_table
+    sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
-        name TEXT, 
+        name TEXT,
         grade TEXT
       )
-  
+      SQL 
+    DB[:conn].execute(sql)
 end
